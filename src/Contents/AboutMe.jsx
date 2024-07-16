25fe1ef9c0fa2a4dc3b56bed6b77409649e3ab94
Julssssssss <gillojulianna12@gmail.com>
2024-07-16 17:18:37 +0800

    content: change the objectives on the aboutMe section

--- src/Contents/AboutMe.jsx
+++ src/Contents/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
       <div className='circle6'/>
       <div className='AboutMe'>
         <div className='AboutHeader'>About Me</div>
-        <div className='Objectives'>I am Julianna Gillo, an individual aiming to create exceptional work that stands out for its quality and impact, with the goal of achieving excelling in programming demonstrating a strong commitment to leveraging skills and continuous learning.</div>
+        <div className='Objectives'>I am Julianna Gillo, an individual aiming to create exceptional work that stands out for its quality and impact, with the goal of excelling in programming that demonstrates a strong commitment to leveraging skills and continuous learning.</div>
       </div>
     </div>
     </>
